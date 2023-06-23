a05ed3451fe2597352a5952a2d320f43a5e779b2
Yael Salim Franco <yael.salim@domo.com>
2023-06-23 15:25:19 -0300

    added signup component and form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,26 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+
+import Button from "../../components/button/button.component";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import {
+  //   auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  //   signInWithGoogleRedirect,
 } from "../../utils/firebase.utils";
 
 const SignIn = () => {
+  //   useEffect(() => {
+  //     const getRedirectRes = async () => {
+  //       const response = await getRedirectResult(auth);
+  //       if (response) {
+  //         const userDocRef = await createUserDocumentFromAuth(response.user);
+  //       }
+  //     };
+  //     getRedirectRes();
+  //   }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -12,7 +29,13 @@ const SignIn = () => {
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign In with Google Popup </button>
+      <Button buttonType="google" onClick={logGoogleUser}>
+        Sign In with Google Popup{" "}
+      </Button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign In with Google Redirect{" "}
+      </button> */}
+      <SignUpForm />
     </div>
   );
 };
